f005859315dadb057e7cb5500c66e71f5d7f5a26
devleopar <oktarkara@gmail.com>
2020-08-01 18:33:48 +0300

    json send routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -40,23 +42,56 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if(!req.query.address){
+    return res.send({
+      error: 'You must provide an address!'
+    })
+  }
+  geocode(req.query.address, (error, { latitude, longtitude, location } = {}) => {
+    if (error) {
+      return res.send({error})
+    }
+
+    forecast( latitude, longtitude , (error, forecastData) => {
+      if (error) {
+        return res.send({error})
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      });
+    });
+  });
+
+
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
   res.send({
-    forecast: "Partly Cloudly",
-    location: "Philedelhpia",
+    products: [],
   });
 });
 
 app.get("/help/*", (req, res) => {
   res.render("404", {
-      title:'404',
-      name: "Shoptar",
-      errMsg: "Help Article Not Found",
+    title: "404",
+    name: "Shoptar",
+    errMsg: "Help Article Not Found",
   });
 });
 
 app.get("*", (req, res) => {
   res.render("404", {
-    title:'404',
+    title: "404",
     name: "Shoptar",
     errMsg: "Page Not Found",
   });

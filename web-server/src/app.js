08222c5639938eccc5a8c09d8e7fa6a69c43f796
devleopar <oktarkara@gmail.com>
2020-08-01 11:24:31 +0300

    express installed

---
+++ web-server/src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("<h1>Weather</h1>");
+});
+
+app.get("/help", (req, res) => {
+  res.send([
+    {
+      name: "Andrew",
+    },
+    {
+      name: "Jen",
+    },
+  ]);
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "Partly Cloudly",
+    location: "Philedelhpia",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});

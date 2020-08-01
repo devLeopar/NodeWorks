ef3611b6c096c9746547237c6c8d69710742ac8f
devleopar <oktarkara@gmail.com>
2020-08-01 13:18:06 +0300

    hbs views changed to html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,9 +6,29 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 // const aboutDirectoryPath = path.join(__dirname, "../public/about.html");
 // const helpDirectoryPath = path.join(__dirname, "../public/help.html");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Andrew Mead",
+  });
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Cengiz",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    msg: "How can i help you?",
+    title: "Help Page",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({

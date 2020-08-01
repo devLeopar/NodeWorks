6683950d1c667bd0a348f9bc27b438b31cbcfcf1
devleopar <oktarkara@gmail.com>
2020-08-01 19:36:29 +0300

    weather app front end finished

---
+++ playground/7-default-params.js
@@ -0,0 +1,7 @@
+const greeter = (name = 'James', age) => {
+  console.log("Hello " + name);
+};
+
+greeter('Andrew');
+
+greeter()
--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,32 @@
 console.log("Client side javascript file is loaded");
+
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (!data.error) {
+          messageOne.innerHTML = data.forecast;
+          messageTwo.innerHTML = data.address;
+        } else {
+          messageOne.innerHTML = "There was an error";
+          messageTwo.innerHTML = data.error;
+        }
+      });
+    }
+  );
+});

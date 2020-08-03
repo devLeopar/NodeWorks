f8637c5a419d2c2f57fbdfb8a6045a40b4585e83
devleopar <oktarkara@gmail.com>
2020-08-03 22:00:49 +0300

    update operation finished

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,5 @@
 // CRUD create read update and delete
 
-// const mongodb = require("mongodb");
-// const MongoClient = mongodb.MongoClient;
-// const ObjectID = mongodb.ObjectID;
-
 const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
@@ -19,42 +15,29 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").findOne(
-    //   { _id: new ObjectID("5f2826cc4456921d1ca247ee") },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log("Unable to fetch");
+    // db.collection("users").updateOne(
+    //   {
+    //     _id: new ObjectID("5f2820b8f2a38817d013c7bd"),
+    //   },
+    //   {
+    //     $inc:{
+    //       age:1
     //     }
-
-    //     console.log(user);
     //   }
-    // );
-
-    // db.collection("users")
-    //   .find({ age: 27 })
-    //   .count((error, count) => {
-    //     console.log(count);
+    // ).then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
     //   });
 
-    // db.collection("tasks").findOne(
-    //   { _id: new ObjectID("5f2823c6ddf6363d88ca2ef5") },
-    //   (error, task) => {
-    //     if (error) {
-    //       return console.log(error);
-    //     }
-
-    //     console.log(task);
-    //   }
-    // );
-
-    // db.collection("tasks")
-    //   .find({ completed: false })
-    //   .toArray((error, tasks) => {
-    //     if (error) {
-    //       return console.log(error);
-    //     }
-
-    //     console.log(tasks);
-    //   });
+    db.collection("tasks").updateMany(
+      { completed: { $eq: false } },
+      { $set: { completed: true } }
+    ).then((result) => {
+      console.log(result)
+    }).catch((e) => {
+      console.log(e)
+    });
   }
 );

62b4c1904c0d1ea31a7f08c66910815e06909850
devleopar <oktarkara@gmail.com>
2020-08-06 16:33:42 +0300

    mongo validation applied

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,47 +1,68 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology:true
+  useUnifiedTopology: true
 });
 
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true
+  },
+  email:{
+    type: String,
+    required: true,
+    trim: true,
+    lowercase:true,
+    validate(value){
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
   },
   age: {
     type: Number,
+    default:0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
   },
 });
 
-// const me = new User({
-//   name: "Andrew",
-//   age: "Mike",
-// });
+const me = new User({
+  name: '    Andrew    ',
+  email: 'JaMesBondY@gmail.COm   '
+});
 
-// me.save().then((me)=>{
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log('Error! ', error)
+me.save().then((me) => {
+  console.log(me)
+}).catch((error) => {
+  console.log('Error! ', error)
+})
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     completed: {
+//         type: Boolean
+//     }
 // })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-})
+// const routine = new Task({
+//     description: "Check your routine",
+//     completed: false
+// })
 
-const routine = new Task({
-    description: "Check your routine",
-    completed: false
-})
-
-routine.save().then((task)=>{
-    console.log(task)
-}).catch((error)=>{
-    console.log(error)
-})
+// routine.save().then((task)=>{
+//     console.log(task)
+// }).catch((error)=>{
+//     console.log(error)
+// })

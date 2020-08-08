943243817933d0037010acc8ee42bb371fdd2431
devleopar <oktarkara@gmail.com>
2020-08-08 13:27:45 +0300

    async await was implied to ease promise chaining process

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete('5f2d3f42b011f658cc19c546').then((task)=>{
-    console.log(task)
-    return Task.countDocuments({ completed:false })
-}).then((result)=>{
-    console.log(result)
+// Task.findByIdAndDelete('5f2d3f42b011f658cc19c546').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({ completed:false })
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed:false})
+    return count
+}
+
+deleteTaskAndCount("5f2d3efd091d9c1038e40215").then((count)=>{
+    console.log(count);
 }).catch((e)=>{
     console.log(e)
 })

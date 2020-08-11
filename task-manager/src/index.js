c8a3179921c5ebb8629a5096ca81ad64475a48d8
devleopar <oktarkara@gmail.com>
2020-08-11 21:20:51 +0300

    avatar image will be added

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,17 +8,16 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req,res,next)=>{
-//     if (req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req,res,next)=>{
-//     res.status(503).send('Service are under maintanence')
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload',upload.single('upload'), (req,res)=>{
+    res.send()
+})
+
 
 app.use(express.json());
 app.use(userRouter);

2e7261dc3ef9cd80ca53901a7e5ce9e17a8bc6af
devleopar <oktarkara@gmail.com>
2020-08-11 21:43:49 +0300

    file uploads are allowed with multer

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -87,7 +87,18 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload a picture'))
+        }
+
+        cb(undefined,true)
+
+    }
 })
 
 router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{

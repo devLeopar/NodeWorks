7ba114f0d1e1c4d14fabf16b06fb7e110d1de8c3
devleopar <oktarkara@gmail.com>
2020-08-13 18:37:38 +0300

    user profile picture is attached

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,27 +9,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req,file,cb){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a word document'))
-        }
-
-        cb(undefined,true)
-
-    }
-})
-
-app.post('/upload',upload.single('upload'), (req,res)=>{
-    res.send()
-})
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
